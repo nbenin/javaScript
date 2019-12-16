d5351cd9b1d066c9f46a401f9c6cec001b615a05
Neil <neil.mf.benin@gmail.com>
2019-12-16 12:33:13 +0100

    2.1 done

--- js-complete-course/02-maths/01-calculator-one/script.js
+++ js-complete-course/02-maths/01-calculator-one/script.js
@@ -14,17 +14,41 @@
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        let firstArgument = document.getElementById("op-one").value;
+        let secondArgument = document.getElementById("op-two").value;
+
+        let result = Number(firstArgument) + Number(secondArgument);
+
+        alert(result);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        let firstArgument = document.getElementById("op-one").value;
+        let secondArgument = document.getElementById("op-two").value;
+
+        let result = Number(firstArgument) - Number(secondArgument);
+
+        alert(result);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        let firstArgument = document.getElementById("op-one").value;
+        let secondArgument = document.getElementById("op-two").value;
+
+        let result = Number(firstArgument) * Number(secondArgument);
+
+        alert(result);
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        let firstArgument = document.getElementById("op-one").value;
+        let secondArgument = document.getElementById("op-two").value;
+
+        let result = Number(firstArgument) / Number(secondArgument);
+
+        alert(result);
     });
 })();
