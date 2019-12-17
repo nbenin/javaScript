d2daca0ea2e7bccee6b26f1d84c1b3559614df48
Neil Benin <neil.mf.benin@gmail.com>
2019-12-17 15:12:17 +0100

    done, just before break, cool random math function

--- js-complete-course/03-colors/03-change-bcg-three/script.js
+++ js-complete-course/03-colors/03-change-bcg-three/script.js
@@ -11,6 +11,9 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function() {
+       let randomBackgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = randomBackgroundColor;
+    });
 
 })();
