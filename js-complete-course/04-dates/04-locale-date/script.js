8d7b2e8ea6e4c2712a84574af4010827f136ff02
Neil Benin <neil.mf.benin@gmail.com>
2019-12-19 11:58:56 +0100

    4.4 finished, used 2 switch statements

--- js-complete-course/04-dates/04-locale-date/script.js
+++ js-complete-course/04-dates/04-locale-date/script.js
@@ -13,15 +13,10 @@
 
     // Get date and extract values
     let currentDate = new Date();
-    let currentNameOfDay = nameOfDay(currentDate.getDay());
-    let currentNumberOfDay = currentDate.getDate();
-    let currentMonth = nameOfMonth(currentDate.getMonth());
 
-
-
-    document.getElementById("target").innerHTML = currentDate;
-
-    // your code here
+    document.getElementById("target").innerHTML = nameOfDay(currentDate.getDay()) + " " + currentDate.getDate()
+        + " " + nameOfMonth(currentDate.getMonth()) + " " + currentDate.getFullYear() + ", " + currentDate.getHours()
+        + "h" + currentDate.getMinutes();
 
 })();
 
@@ -53,6 +48,46 @@ function nameOfDay(x) {
 }
 
 // Function to get name of Month
+function nameOfMonth(y) {
+    switch (y) {
+        case 0:
+            return "January";
+            break;
+        case 1:
+            return "February";
+            break;
+        case 2:
+            return "March";
+            break;
+        case 3:
+            return "April";
+            break;
+        case 4:
+            return "May";
+            break;
+        case 5:
+            return "June";
+            break;
+        case 6:
+            return "July";
+            break;
+        case 7:
+            return "August";
+            break;
+        case 8:
+            return "September";
+            break;
+        case 9:
+            return "October";
+            break;
+        case 10:
+            return "November";
+            break;
+        case 11:
+            return "December";
+            break;
 
+    }
+}
 
 
