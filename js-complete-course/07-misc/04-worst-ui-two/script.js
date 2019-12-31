549c662566a69b86ae8c982012b38b5f5dd14c1a
Neil Benin <neil.mf.benin@gmail.com>
2019-12-31 17:17:46 +0100

    almost got solution i think

--- js-complete-course/07-misc/04-worst-ui-two/script.js
+++ js-complete-course/07-misc/04-worst-ui-two/script.js
@@ -14,49 +14,75 @@
     // adding event listeners to buttons
     document.getElementById('part-one').addEventListener('click', function() {
 
-        // set new value, if 500, reset to 460
+        // set new value, if above data-max, reset to data-min
         let newInnerValue = parseInt(document.getElementById('part-one').innerHTML, 10) + 1;
         if (newInnerValue === 500) {
             newInnerValue = 460;
         }
 
-        // if under 10, prepend with a zero, else set innerHTML and parse to string for later concatenation
+        // convert number to string for later concatenation. If under 10, add a zero before
+        newInnerValue += "";
         if (newInnerValue.length < 2) {
-            newInnerValue += "0";
+            newInnerValue = "0" + newInnerValue;
         }
-        document.getElementById('part-one').innerHTML = newInnerValue.toString(10);
 
+        // add new value to page
+        document.getElementById('part-one').innerHTML = newInnerValue;
 
     });
     document.getElementById('part-two').addEventListener('click', function() {
+
         let newInnerValue = parseInt(document.getElementById('part-two').innerHTML, 10) + 1;
         if (newInnerValue === 100) {
             newInnerValue = 0;
         }
+
+        newInnerValue += "";
         if (newInnerValue.length < 2) {
-            newInnerValue += "0";
+            newInnerValue = "0" + newInnerValue;
         }
-        document.getElementById('part-two').innerHTML = newInnerValue.toString(10);
+
+        document.getElementById('part-two').innerHTML = newInnerValue;
     });
     document.getElementById('part-three').addEventListener('click', function() {
+
         let newInnerValue = parseInt(document.getElementById('part-three').innerHTML, 10) + 1;
         if (newInnerValue === 100) {
             newInnerValue = 0;
         }
+
+        newInnerValue += "";
         if (newInnerValue.length < 2) {
-            newInnerValue += "0";
+            newInnerValue = "0" + newInnerValue;
         }
-        document.getElementById('part-three').innerHTML = newInnerValue.toString(10);
+
+        document.getElementById('part-three').innerHTML = newInnerValue;
     });
     document.getElementById('part-four').addEventListener('click', function() {
+
         let newInnerValue = parseInt(document.getElementById('part-four').innerHTML, 10) + 1;
         if (newInnerValue === 100) {
             newInnerValue = 0;
         }
+
+        newInnerValue += "";
         if (newInnerValue.length < 2) {
-            newInnerValue += "0";
+            newInnerValue = "0" + newInnerValue;
         }
-        document.getElementById('part-four').innerHTML = newInnerValue.toString(10);
+
+        document.getElementById('part-four').innerHTML = newInnerValue;
+    });
+
+    // Add listener to the whole button section, on change update info in target
+    document.getElementsByClassName('actions')[0].addEventListener('change', function() {
+
+        // get values from the buttons
+        let firstValue = document.getElementById('part-one').innerHTML;
+        let secondValue = document.getElementById('part-two').innerHTML;
+        let thirdValue = document.getElementById('part-three').innerHTML;
+        let fourthValue = document.getElementById('part-four').innerHTML;
+
+        document.getElementById('target').innerHTML = firstValue + secondValue + thirdValue + fourthValue;
     });
 
 })();
