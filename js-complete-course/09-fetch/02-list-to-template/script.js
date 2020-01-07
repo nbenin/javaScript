48411ce13795f146c874d2b63977d2529e2c0393
Neil <neil.mf.benin@gmail.com>
2020-01-07 17:01:18 +0100

    going home

--- js-complete-course/09-fetch/02-list-to-template/script.js
+++ js-complete-course/09-fetch/02-list-to-template/script.js
@@ -20,12 +20,9 @@
                 return response.json();
             })
             .then(data => {
-                return data.heroes;
-            })
-            .then(objects => {
 
                 // loop through and append template
-                for (let obj in objects) {
+                for (let obj in data.heroes) {
 
                     // add all templated list nodes
                     let listNode = document.importNode(xmenTemp.content.querySelector('li'), true);
@@ -38,18 +35,16 @@
                 let powersNode = document.getElementsByClassName('powers');
 
                 // Loop through heroes and add info
-                for (let obj in objects) {
-                    nameNode[obj].textContent += 'Name: ' + objects[obj].name;
-                    alterEgoNode[obj].textContent += 'Alter-Ego: ' + objects[obj].alterEgo;
+                for (let obj in data.heroes) {
+                    nameNode[obj].innerHTML += data.heroes[obj].name + '</br>';
+                    alterEgoNode[obj].innerHTML += data.heroes[obj].alterEgo;
                     powersNode[obj].textContent += 'Powers: ';
 
                     // loop for abilities
-                    for (let pow = 0; pow < objects[obj].abilities.length; pow++) {
-                        powersNode[obj].innerHTML += objects[obj].abilities[pow] + '</br>';
-
+                    for (let pow = 0; pow < data.heroes[obj].abilities.length; pow++) {
+                        powersNode[obj].innerHTML += data.heroes[obj].abilities[pow] + '</br>';
                     }
                 }
-
             });
     });
 })();
--- js-complete-course/09-fetch/03-details/script.js
+++ js-complete-course/09-fetch/03-details/script.js
@@ -10,5 +10,42 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById('run').addEventListener('click', function() {
+
+        // get json
+        fetch("../../_shared/api.json")
+            .then(response => {
+                return response.json();
+            })
+            .then(heroList => {
+
+                // Get variables that we will use often
+                let heroName = document.getElementById('hero-id').value;
+                let xmenTemplate = document.getElementById('tpl-hero');
+                let xmenTemplateClone = xmenTemplate.content.cloneNode(true);
+                let heroToBeShown = null;
+
+                // set hero to be shown as a vairbale
+                for (let hero in heroList.heroes) {
+                    if (heroName == heroList.heroes[hero].name ) {
+                        heroToBeShown = heroList.heroes[hero];
+                        console.log(heroToBeShown);
+                    }
+                }
+
+                // error check
+                if (heroToBeShown === null) {
+                    alert('choose from: Cyclops, Marvel Girl, Beast, Angel, or Iceman');
+                    return;
+                }
+
+                // using the clone to set inner HTML
+                xmenTemplateClone.querySelector('.name').innerHTML = 'Name: ' + heroToBeShown.name;
+                xmenTemplateClone.querySelector('.alter-ego').innerHTML = 'Alter-Ego: ' + heroToBeShown.alterEgo;
+                xmenTemplateClone.querySelector('.powers').innerHTML = 'Powers: ' + heroToBeShown.abilities;
+
+                document.getElementById('target').appendChild(xmenTemplateClone);
+
+            });
+    });
 })();
