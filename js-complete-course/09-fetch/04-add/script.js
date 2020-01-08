b3e36984b9a490017bfd8c5b1aef3b59cfd24ae6
Neil <neil.mf.benin@gmail.com>
2020-01-08 10:50:43 +0100

    9.4 done, made a hero class (thanks emma! ;))

--- js-complete-course/09-fetch/04-add/script.js
+++ js-complete-course/09-fetch/04-add/script.js
@@ -10,5 +10,42 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    // event listener
+    document.getElementById('run').addEventListener('click', function() {
+
+        // on click, fetch api and read to console
+        fetch('../../_shared/api.json')
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                return data.heroes;
+            })
+            .then(objects => {
+
+                // get values and store in variable
+                let newHeroName = document.getElementById('hero-name').value;
+                let newHeroAlterEgo = document.getElementById('hero-alter-ego').value;
+                let newHeroPowers = document.getElementById('hero-powers').value.split(",");
+                let newHeroId = objects.length + 1;
+
+                // make new hero using class and push to array
+                let userCreatedHero = new Hero(newHeroId,  newHeroName, newHeroAlterEgo, newHeroPowers);
+                objects.push(userCreatedHero);
+                console.log(objects);
+
+            });
+    });
 })();
+
+// making a class for New Heroes (thanks emma ;))
+class Hero {
+    constructor(heroId, heroName, heroAlterEgo, heroAbilities) {
+        this.id = heroId;
+        this.name = heroName;
+        this.alterEgo = heroAlterEgo;
+        this.abilities = heroAbilities;
+    }
+}
+
